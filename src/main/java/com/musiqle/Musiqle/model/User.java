f016b1c53a89238a5d20f08635c95cca7601befb
Agnes Kim <agneskim4@gmail.com>
2023-07-25 15:05:50 -0400

    initialization controller and entity

---
+++ src/main/java/com/musiqle/Musiqle/model/User.java
@@ -0,0 +1,14 @@
+// package com.musiqle.Musiqle.model;
+
+// import jakarta.persistence.*;
+// import jakarta.persistence.Entity;
+
+// @Entity
+// @Table(name="user")
+// public class User {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private int id;
+
+    
+// }
