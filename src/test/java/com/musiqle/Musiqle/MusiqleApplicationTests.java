5be793e46b236bc99b9d8eae8edc405e1c6bd1a5
Sydney Alberson <sydney.alberson@gmail.com>
2023-07-24 20:59:20 -0400

    first commit

---
+++ src/test/java/com/musiqle/Musiqle/MusiqleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musiqle.Musiqle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusiqleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
