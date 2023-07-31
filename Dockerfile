7143221dbc89fa7cea1c6fb9f0e9ab5283bd1399
Sydney Alberson <sydney.alberson@gmail.com>
2023-07-31 17:33:07 -0400

    Removed comments

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM eclipse-temurin:17-jdk-alpine
-VOLUME /tmp
-COPY target/*.jar musiqle-back-end.jar
-ENTRYPOINT ["java","-jar","/musiqle-back-end.jar"]
-EXPOSE 8080
--- src/main/java/com/musiqle/Musiqle/MusiqleApplication.java
+++ src/main/java/com/musiqle/Musiqle/MusiqleApplication.java
@@ -1,20 +1,9 @@
 package com.musiqle.Musiqle;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.context.annotation.ComponentScan;
 
-// @ComponentScan("com.musiqle.Musiqle.UserRepository")
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-// @SpringBootApplication
-// @Configuration
-// @EnableJpaRepositories(basePackages = "com.musiqle.Musiqle.UserRepository")
-
+@SpringBootApplication
 public class MusiqleApplication {
 
 	public static void main(String[] args) {
