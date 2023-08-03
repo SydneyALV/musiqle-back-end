0f32839492b36f7347bfb64ca10176ee752b9ba0
Agnes Kim <agneskim4@gmail.com>
2023-08-03 09:40:13 -0400

    Update users to include id shown, update jar file

--- src/main/java/com/musiqle/Musiqle/Users.java
+++ src/main/java/com/musiqle/Musiqle/Users.java
@@ -51,6 +51,13 @@ public class Users {
     }
     
     //Getters and Setters 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
     public String getName() {
         return name;
     }
