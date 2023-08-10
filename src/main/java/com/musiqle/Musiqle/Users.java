a237e18d231e2958362420437b0806bec26756d3
Agnes Kim <agneskim4@gmail.com>
2023-08-10 14:13:36 -0400

    Update users entity to show id

--- src/main/java/com/musiqle/Musiqle/Users.java
+++ src/main/java/com/musiqle/Musiqle/Users.java
@@ -41,6 +41,14 @@ public class Users {
     }
     
     //Getters and Setters 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
     public String getName() {
         return name;
     }
